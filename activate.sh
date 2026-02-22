0f894b9c2ab8f6f53e30e31071939dbcffa657b7
Meisoft <38667978+Meisoftcoltd@users.noreply.github.com>
2026-02-22 12:15:30 +0100

    Delete activate.sh

--- activate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "🎵 Activando entorno de procesamiento de audio..."
-cd "$HOME/proyecto"
-source venv_audio/bin/activate
-echo "✅ Entorno activado. Ejecuta: python you2dl_optimizado.py"
